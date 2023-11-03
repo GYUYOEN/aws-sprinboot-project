27b61ae8493f4949138ae4180e7c9c56780a8584
김규연 <rbdus9696@naver.com>
2023-11-04 01:35:04 +0900

    1. 창조날짜와 수정날짜 자동 생성을 위한 BaseTimeEntity 생성 2. 위내용 테스트 코드 실행

--- src/main/java/org/example/springboot/Application.java
+++ src/main/java/org/example/springboot/Application.java
@@ -2,7 +2,9 @@ package org.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 어노테이션들을 모두 활성화 할 수 있도록 하는 어노테이션
 // 이 어노테이션이 있는 위치부터 설정을 읽어감. 따라서 이 클래스는 항상 프로젝트의 최상단에 위치해야 함.
 @SpringBootApplication // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 public class Application {
