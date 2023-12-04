e4a3dcefedab812cef281a318e9fff8b15875476
김규연 <rbdus9696@naver.com>
2023-12-04 14:02:32 +0900

    로그인 세션 코드 간단화를 위한 LoginUser 어노테이션 개발

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.LoginUser;
 import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
@@ -19,14 +20,14 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
         /*
         앞서 작성된 CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
         즉, 로그인 성공 시 httpSession.getAttribute("user") 에서 값을 가져올 수 있음
          */
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user"); -> @LoginUser로 대체
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
